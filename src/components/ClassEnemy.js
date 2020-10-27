29f62d75a2c3b59ccbcd4085c9b908edb01b9320
Francisco Javier Vivas Astudillo <francisco.vivas95@gmail.com>
2020-10-27 13:34:24 -0500

    Enemy class done

--- src/components/ClassEnemy.js
+++ src/components/ClassEnemy.js
@@ -13,7 +13,7 @@ class Enemy {
     draw() {
         this.y += this.gravity;
         if (this.x > $canvas.width - this.width) { this.x = $canvas.width - this.width; }
-        else if (this.x < 0) { this.x = 0 }
+        else if (this.x < 0) { this.x = 0; }
         else { this.x = Math.cos(this.freq * frames) * (this.width / 10) + this.x; }
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const $canvas = document.querySelector('canvas');
 const ctx = $canvas.getContext('2d');
 
 let intervalId;
+let frames = 0;
+
+let enemiesArr = [];
+let enemiesCount = 2;
 
 document.querySelector('#start').onclick = start;
 
@@ -12,11 +16,16 @@ function start() {
 }
 
 function update() {
+    frames++;
     // 1. Calc.
+    generateEnemies();
 
     //2. Clear
+    cleanEnemies();
     clearCanvas();
     //3. Draw
+    drawEnemies();
+
 
     intervalId = setTimeout(requestAnimationFrame(update), 1000 / 60);
 }
@@ -25,4 +34,29 @@ function update() {
 
 function clearCanvas() {
     ctx.clearRect(0, 0, $canvas.width, $canvas.height);
+}
+
+function generateEnemies() {
+    if (enemiesArr.length < enemiesCount) {
+        let initWidth = randomNumber(55, 15);
+        let initHeight = randomNumber(10, 5);
+        let initX = randomNumber($canvas.width - initWidth, initWidth);
+        let initY = - randomNumber(initHeight);
+        let initFreq = randomNumber(0.2, 0.04, false);
+        let initGravity = randomNumber(0.34, 0.4, false);
+        enemiesArr.push(new Enemy(initX, initY, initWidth, initHeight, initFreq, initGravity));
+    }
+}
+
+function drawEnemies() {
+    enemiesArr.forEach(e => e.draw());
+}
+
+function randomNumber(range = 1, offset = 0, isRoundNeeded = true) {
+    if (isRoundNeeded) return Math.floor((Math.random() * range) + offset);
+    return (Math.random() * range) + offset;
+}
+
+function cleanEnemies() {
+    enemiesArr = enemiesArr.filter(e => e.y <= $canvas.height);
 }
